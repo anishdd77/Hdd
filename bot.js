665b584c755dc36f447c3212c2b18d516525f698
anishdd77 <51217167+anishdd77@users.noreply.github.com>
2019-06-14 22:47:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var fs = require('fs');
 var client = new Discord.Client();
+var Discord = require('discord.js');
 
 client.on('ready', () => {
   console.log(`Welcome in servr Anis ${client.user.tag}!`);
