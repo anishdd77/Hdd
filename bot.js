652455dde2dea965b81a517dc5602800958bab57
anishdd77 <51217167+anishdd77@users.noreply.github.com>
2019-06-27 14:11:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,6 +113,11 @@ if (message.content.startsWith(prefix + 'help')) { //Anis_hdd - [ ANIS_Malumuat]
 ✴${prefix}hchannel > اخفاء الشات
 ✴${prefix}schannel > اضهار الشات المخفية
 ✴${prefix}ct >لعمل روم كتابي
+✴#setvoice
+✴#setmember
+✴#settime
+✴#setdate
+✴#setdays
 ✴${prefix}cv >وم كتابي صوتي
 ----------------------
 ✴#guilds : عدد سيرفر البوت
@@ -2105,5 +2110,128 @@ client.on('message', message => {
                  message.channel.sendEmbed(Date15);
         }
     });
+
+client.on('message',async message => {
+ 
+  if(message.content.startsWith(prefix + "setvoice")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(function() {
+      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+    },1000);
+  });
+  }
+});
+ 
+client.on('message',async message => {
+  if(message.content.startsWith(prefix + "setmember")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Members Count : [ ${message.guild.members.size} ]` , 'voice').then(c => {
+    console.log(`Count Members channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(function() {
+      c.setName(`Members Count : [ ${message.guild.members.size} ]`)
+    },1000);
+  });
+  }
+});
+ 
+ 
+client.on('message',async message => {
+  if(message.content.startsWith(prefix + "settime")) {
+  if(!message.guild.member(message.author).hasPermission('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermission(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel("🕐 - Time  00", 'voice').then((c) => {
+    console.log(`Time channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });//ToxicCodes CopyRights
+        setInterval(function() {
+ 
+      var currentTime = new Date(),
+      hours = currentTime.getHours() + 3 ,
+      minutes = currentTime.getMinutes(),
+      seconds = currentTime.getSeconds(),
+      years = currentTime.getFullYear(),
+      month = currentTime.getMonth(),
+      day = currentTime.getDate(),
+      week = currentTime.getDay();
+ 
+      if (minutes < 10) {
+          minutes = "0" + minutes;
+      }
+      var suffix = "AM";
+      if (hours >= 12) {
+          suffix = "PM";
+          hours = hours - 12;
+      }
+      if (hours == 0) {
+          hours = 12;
+      }
+ 
+      c.setName("🕐 - Time   「" + hours + ":" + minutes  +" " + suffix + "」");
+    },1000);
+  });
+  }
+});
+ 
+ 
+ 
+client.on('message',async message => {
+  if(message.content.startsWith(prefix + "setdate")) {
+      var currentTime = new Date(),
+      years = currentTime.getFullYear(),
+      month = currentTime.getMonth() + 1,
+      day = currentTime.getDate(),
+      week = currentTime.getDay();
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel("📅 - Date " + "「" + day + "-" + month + "-" + years + "」" , 'voice').then(c => {
+    console.log(`Date channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(function() {
+      c.setName("📅 - Date " + "「" + day + "-" + month + "-" + years + "」")
+    },1000);
+  });
+  }
+});
+ 
+ 
+client.on('message',async message => {
+  var moment = require('moment');
+    if(message.content.startsWith(prefix + "setdays")) {
+    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+    message.channel.send('✅| **تم عمل الروم بنجاح**');
+    message.guild.createChannel(`Day : ${moment().format('dddd')}` , 'voice').then(c => {
+      console.log(`Day channel setup for guild: \n ${message.guild.name}`);
+      c.overwritePermissions(message.guild.id, {
+        CONNECT: false,
+        SPEAK: false
+      });
+      setInterval(function() {
+        c.setName(`📅 - Day : 「${moment().format('dddd')}」`);
+      },1000);
+    });
+    }
+  })
 
 client.login(process.env.BOT_TOKEN)
