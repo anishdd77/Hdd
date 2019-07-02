28f6f4afea62410eabb5654dc1a6205f4bff0da3
anishdd77 <51217167+anishdd77@users.noreply.github.com>
2019-07-02 11:06:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,14 +50,27 @@ if (message.content.startsWith(prefix + 'setgame')) {
     message.channel.sendMessage(`Playing: **${argresult}`)
 } 
 
-
-
 });
 const moment = require('moment')
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('ready', function(){
+    var ms = 60000 ;
+    var setGame = ['help','Type [help]',' [help]'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
+    }, ms);
 client.on('message', message => {
 if (message.content.startsWith(prefix + 'help')) { //Anis_hdd - [ ANIS_Malumuat]
     let pages = [`
